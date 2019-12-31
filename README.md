3a2310610946f2b28f9c5c092814fe3b886c46f3
Dane Naebers <danenaebers@gmail.com>
2019-12-31 11:03:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# common-extensions
+
+![Nuget](https://img.shields.io/nuget/v/DNS.CommonExtensions)
+
+This class library holds some extensions commonly used in various projects build with `ASP.NET`.
---
+++ src/DNS.CommonExtensions/TypeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DNS.CommonExtensions
+{
+    /// <summary>
+    /// <see cref="Type"/> extensions
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Gets the name of the assembly.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Assembly name</returns>
+        public static string GetAssemblyName(this Type type)
+        {
+            return type.Assembly.GetName().Name;
+        }
+
+        /// <summary>
+        /// Gets the namespace root segment.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Namespace root</returns>
+        public static string GetNamespaceRootSegment(this Type type)
+        {
+            return type.Namespace?.Split('.')[0];
+        }
+    }
+}
